5e377bc5da6096a67ccf97a7a331638c69a59bf4
Alexandre Cesar <acmorcego@gmail.com>
2018-04-03 18:42:42 -0300

    Implement board

---
+++ src/component/board.js
@@ -0,0 +1,17 @@
+import { h } from 'preact'
+import styled from 'styled-components'
+
+import Column from './column'
+
+const Layout = styled.div`
+	display: flex;
+	padding: 12px;
+`
+
+const Board = ({ columns }) => (
+	<Layout>
+		{columns.map(({ name, issues }) => <Column name={name} issues={issues} />)}
+	</Layout>
+)
+
+export default Board
