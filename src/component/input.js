47f8a52da41cfbe8d89cbc76cc8a97d8edf5318f
Alexandre Cesar <acmorcego@gmail.com>
2018-04-07 13:34:16 -0300

    Add input component

---
+++ src/component/input.js
@@ -0,0 +1,35 @@
+import { h } from 'preact'
+import styled from 'styled-components'
+
+const Input = styled.input`
+	background: white;
+	border: 1px solid #0001;
+	border-radius: 2px;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
+	display: block;
+	margin-bottom: 4px;
+	padding: 8px;
+`
+
+const Label = styled.label`
+	span {
+		display: block;
+		font-weight: bold;
+		margin-bottom: 4px;
+		margin-left: 8px;
+	}
+	small {
+		color: #0009;
+		margin-left: 8px;
+	}
+`
+
+const LabeledInput = props => (
+  <Label>
+    <span>{props.label}</span>
+    <Input {...props} />
+    {props.description ? <small>{props.description}</small> : null}
+  </Label>
+)
+
+export default LabeledInput
